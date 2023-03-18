f9c397244262cb230591ab562bf526e2551ab1cc
ola9292 <okaz692@gmail.com>
2023-03-18 20:00:50 +0000

    added footer

--- my-app/src/components/Layout.jsx
+++ my-app/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom"
 import Header from "./Header";
+import Footer from "./Footer";
 function Layout(){
     return(
         <div>
             <Header />
             <Outlet />
+            <Footer />
         </div>
     )
 }
